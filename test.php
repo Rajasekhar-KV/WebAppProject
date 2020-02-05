72d72ebdc8b96f4e90b509c18e70ade94958342f
Rajasekhar-KV <55240640+Rajasekhar-KV@users.noreply.github.com>
2020-02-05 18:24:10 +0530

    Update test.php

--- test.php
+++ test.php
@@ -3,7 +3,7 @@ DEFINE ('DB_USER', 'appdbuser');
 DEFINE ('DB_PASSWORD', 'Appdbu$er1');
 DEFINE ('DB_HOST', '127.0.0.1');
 DEFINE ('DB_NAME', 'datadb');
-
+//database connection
 $dbcon = @mysqli_connect (DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) OR die('Could not connect to MySQL: '. mysqli_connect_error());
 
 mysqli_set_charset($dbcon, 'utf8');
